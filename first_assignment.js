799952775d3e8ea2fe6bcedaa37c5cb0b55f6299
mrdeveloper <mrdeveloper6373@gmail.com>
2024-06-29 00:05:51 +0500

    completed

---
+++ first_assignment.js
@@ -0,0 +1,70 @@
+'use strict';
+
+// // first assignment
+
+// const myArray = [1, 2, 3, 4, 5];
+// // removing first two elements
+// myArray.shift();
+// myArray.shift();
+
+// // adding three elements at the end of the array
+// myArray.push(10,20,30);
+
+// // printing the array using loop
+// for (let i = 0; i < myArray.length; i++) {   
+//     console.log(myArray[i]);
+    
+// }
+
+// second assignment
+
+// const anotherArray = [1, 2, 3, 4, 5];
+// for (let i = 0; i < anotherArray.length; i++) {
+//     if (anotherArray[i] %2 == 0) {
+//         console.log(`Index: ${i}`);
+//     }
+    
+// }
+
+
+// // third assignment
+// const thirdArray = [3, 4, 5];
+// // adding two elements at the beginning of the array 
+// thirdArray.unshift(1,2);
+
+// // removing last element of the array
+// thirdArray.pop();
+
+// // printing the array using loop
+// for (let i = 0; i < thirdArray.length; i++) {
+//     console.log(thirdArray[i]);
+    
+// }
+
+
+// fourth Assignment
+
+// const fourthArray =[1, 2, 3, 4, 5];
+// for (let i = 0; i < fourthArray.length; i++) {
+//     if (fourthArray[i] > fourthArray[i-1]) {
+//         console.log(fourthArray[i])
+//     }
+    
+// }
+
+// fifth assignment
+
+// const fifthArray = [1, 2, 3, 4, 5];
+
+// // findin the index of third element and store in a variable
+// const second_index = fifthArray.indexOf(3);
+
+// // removing all the elements starts from that index
+//  const newArray = fifthArray.splice(0, second_index);
+
+//  for (let i = 0; i < newArray.length; i++) {
+//     console.log(newArray[i]);
+    
+//  }
+
+ 
